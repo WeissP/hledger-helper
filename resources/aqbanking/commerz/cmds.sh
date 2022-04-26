d4fd52e4ee9dd0ac5e2212374f92408843bd48b4
weiss <kingbaiing@163.com>
2022-04-26 23:02:24 +0200

    update cmds

--- resources/aqbanking/commerz/cmds.sh
+++ resources/aqbanking/commerz/cmds.sh
@@ -4,7 +4,7 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 source ./env.sh 
 
-aqbanking-cli -P pinfile request --number=8 -b 54040042 --transactions -c $temp --fromdate=$fromdate --todate=$todate
+aqbanking-cli -P pinfile request -b 54040042 --transactions -c $temp --fromdate=$fromdate --todate=$todate
 
     
 
--- resources/aqbanking/paypal/cmds.sh
+++ resources/aqbanking/paypal/cmds.sh
@@ -4,7 +4,7 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 source ./env.sh 
 
-aqbanking-cli request --number=8 --aid=3 --transactions -c $temp --fromdate=$fromdate --todate=$todate
+aqbanking-cli request --aid=3 --transactions -c $temp --fromdate=$fromdate --todate=$todate
 
 
     
